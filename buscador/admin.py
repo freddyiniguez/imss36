e53d22f6176e3f1b005950c7f2185508c2626e20
Freddy Iñiguez <iniguez.devt@gmail.com>
2018-03-14 01:05:57 -0600

    Importing excel file is working but without special characters.

--- buscador/admin.py
+++ buscador/admin.py
@@ -2,6 +2,60 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from import_export.admin import ImportMixin
+from import_export import resources, fields
 from .models import Cita
 
-admin.site.register(Cita)
+class CitaResource(resources.ModelResource):
+	fecha_relacion = fields.Field(
+		attribute = 'fecha_relacion',
+		column_name = 'FECHA DE RELACION'
+	)
+
+	fecha_recibo = fields.Field(
+		attribute = 'fecha_recibo',
+		column_name = 'FECHA DE RECIBIDO'
+	)
+
+	nombre = fields.Field(
+		attribute = 'nombre',
+		column_name = 'NOMBRE DEL PACIENTE'
+	)
+
+	nss = fields.Field(
+		attribute = 'nss',
+		column_name = 'No. DE AFILIACION'
+	)
+
+	dx = fields.Field(
+		attribute = 'dx',
+		column_name = 'DIAGNOSTICO'
+	)
+
+	lugar = fields.Field(
+		attribute = 'lugar',
+		column_name = 'LUGAR'
+	)
+
+	especialidad = fields.Field(
+		attribute = 'especialidad',
+		column_name = 'ESPECIALIDAD'
+	)
+
+	fecha_cita = fields.Field(
+		attribute = 'fecha_cita',
+		column_name = 'FECHA DE CITA'
+	)
+
+	def get_instance(self, instance_loader, row):
+		return False
+
+	class Meta:
+		model = Cita
+		fields = ('fecha_relacion', 'fecha_recibo', 'nombre', 'nss', 'dx', 'lugar', 'especialidad', 'fecha_cita')
+		export_order = fields
+
+class CitaAdmin(ImportMixin, admin.ModelAdmin):
+	resource_class = CitaResource
+
+admin.site.register(Cita, CitaAdmin)
--- buscador/models.py
+++ buscador/models.py
@@ -6,15 +6,15 @@ from django.db import models
 # - - - C I T A - - - #
 # Contiene informacion de la cita medica
 class Cita(models.Model):
-	fecha_relacion = models.DateField()
-	fecha_recibo = models.IntegerField()
+	fecha_relacion = models.DateTimeField()
+	fecha_recibo = models.DateTimeField()
 	nombre = models.CharField(max_length = 100)
 	nss = models.CharField(max_length = 50)
-	dx = models.CharField(max_length = 60)
-	lugar = models.CharField(max_length = 100, blank=True)
-	especialidad = models.CharField(max_length = 100, blank=True)
-	fecha_cita = models.CharField(max_length = 100, blank=True)
-	status = models.CharField(max_length = 50, blank=True)
+	dx = models.CharField(max_length = 200)
+	lugar = models.CharField(max_length = 200, blank=True)
+	especialidad = models.CharField(max_length = 200, blank=True)
+	fecha_cita = models.TextField(blank=True)
+	status = models.CharField(max_length = 250, blank=True)
 
 	def __str__(self):
 		return self.nombre
